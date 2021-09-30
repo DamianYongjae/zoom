cef89659ac9679882bec474db9457a8cadaec906
DamianYongjae <klick214@gmail.com>
2021-09-30 11:11:42 -0400

    add form and accepts multiple socket and send messages to all browser

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -5,13 +8,18 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New messsage: ", message.data, " from the server");
+  console.log("New messsage: ", message.data);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 });
 
-setTimeout(() => {
-  socket.send("Hello from the browser!");
-}, 10000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -17,11 +17,17 @@ const handleListen = () =>
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Disconnected from the browser"));
-  socket.on("message", (message) => console.log(message.toString("utf8")));
-  socket.send("hello!");
+  socket.on("message", (message) => {
+    // console.log(message.toString("utf8"));
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+    // socket.send(message.toString("utf8"));
+  });
 });
 
 server.listen(3000, handleListen);
