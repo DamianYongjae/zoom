5e9494c326a36fac038e4b18efce674a98eb6ce3
DamianYongjae <klick214@gmail.com>
2022-03-17 17:03:08 -0400

    3.8 Sneders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -88,6 +88,15 @@ function handleCameraOffClick() {
 
 async function handleCameraChange() {
   await getMedia(cameraSelect.value);
+
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 muteButton.addEventListener("click", handleMuteClick);
